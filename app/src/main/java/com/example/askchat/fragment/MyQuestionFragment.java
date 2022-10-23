35405e19ca7812906d0269df0571cb5405b7e57d
Kai Yuen Kelvin Lam <kelvin.kaiyuen@gmail.com>
2022-10-23 22:23:46 +0800

    finished my question fragment

--- app/src/main/java/com/example/askchat/fragment/MyQuestionFragment.java
+++ app/src/main/java/com/example/askchat/fragment/MyQuestionFragment.java
@@ -5,14 +5,30 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.askchat.R;
+import com.example.askchat.fragment.homefunc.PostAdapter;
+import com.example.askchat.fragment.homefunc.PostModel;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyQuestionFragment extends Fragment {
+    RecyclerView recyclerView;
+    PostAdapter postAdapter;
+    List<PostModel> listPost;
 
     public MyQuestionFragment() {
         // Required empty public constructor
@@ -21,12 +37,47 @@ public class MyQuestionFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_question, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_my_question, container, false);
+        init_view(view);
+        return view;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
     }
+
+    private void init_view(View view) {
+        recyclerView = view.findViewById(R.id.fragment_my_question_recyclerView);
+        listPost = new ArrayList<>();
+        postAdapter = new PostAdapter(listPost);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity().getApplicationContext()));
+        recyclerView.setAdapter(postAdapter);
+        readPost();
+    }
+
+    private void readPost() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Posts");
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                listPost.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    PostModel postModel = dataSnapshot.getValue(PostModel.class);
+                    if (postModel.getPublisher().equals(FirebaseAuth.getInstance().getCurrentUser().getUid())) {
+                        listPost.add(postModel);
+                    }
+                }
+                postAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
 }
